39c49f9746b58f56986d1d14cc8de87aa658a722
johnfu <42281520@qq.com>
2015-09-11 20:54:52 +0800

    udp echo server&client

--- test/udp/client.cpp
+++ test/udp/client.cpp
@@ -4,98 +4,112 @@
  *  Created on: 2015年9月8日
  *      Author: ryanbai
  */
+
+//#include <arpa/inet.h>
+#include <assert.h>
+//#include <cygwin/in.h>
+//#include <cygwin/socket.h>
+#include <stdio.h>
+#include <string.h>
+#include <sys/socket.h>
+#include <sys/time.h>
+
 #include "lwip/tcp.h"
 #include "lwip/tcp_impl.h"
 #include "lwip/memp.h"
 #include "lwip/pbuf.h"
 #include "lwip/init.h"
 
-#include <sys/socket.h>
-//#include <netinet/in.h>
-#include <netinet/ip.h> /* superset of previous */
-#include <arpa/inet.h>
-#include <stdio.h>
-#include <string.h>
+#include "common.h"
 
 err_t connected(void *arg, struct tcp_pcb *tpcb, err_t err);
+err_t
+echo_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err);
 
+extern struct sockaddr_in remaddr;      /* server address */
 
-int fd = -1;
-struct sockaddr_in svraddr;      /* server address */
-
+bool run_flag = true;
 int main(int argc, const char* argv[])
 {
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd < 0)
-	{
-		printf("cannot create socket\n");
-		return 0;
-	}
+    int fd = init_udp();
+    if (fd < 0)
+        return 1;
 
-	/* bind the socket to any valid IP address and a specific port */
-	memset((char *)&svraddr, 0, sizeof(svraddr));
-	svraddr.sin_family = AF_INET;
-	svraddr.sin_addr.s_addr = inet_addr("10.12.21.11");
-	svraddr.sin_port = htons(443);
+    init_tcp_stack();
 
-	//初始化
-	lwip_init();
+//	//初始化
+//	lwip_init();
 
 	//
 	struct tcp_pcb *client = tcp_new();
-	client->local_ip.addr = inet_addr("10.12.21.11");
+//	tcp_arg(client, s);
+	tcp_recv(client, echo_recv);
+//	tcp_err(client, smtp_tcp_err);
+//	tcp_poll(client, smtp_tcp_poll, SMTP_POLL_INTERVAL);
+//	tcp_sent(client, smtp_tcp_sent);
+
+
+	/* bind the socket to any valid IP address and a specific port */
+	memset((char *)&remaddr, 0, sizeof(remaddr));
+	remaddr.sin_family = AF_INET;
+	remaddr.sin_addr.s_addr = inet_addr("10.12.21.11");
+	remaddr.sin_port = htons(448);
+
+	client->local_ip.addr = remaddr.sin_addr.s_addr;
 	ip_addr_t ipaddr;
-	ipaddr.addr = svraddr.sin_addr.s_addr;
-	tcp_connect(client, &ipaddr, 443, connected);
+	ipaddr.addr = remaddr.sin_addr.s_addr;
+	tcp_connect(client, &ipaddr, 448, connected);
 
-	/* now loop, receiving data and printing what we received */
-	const int BUFSIZE = 64*1024;
-	char buf[BUFSIZE];
-	for (;;) {
-		struct sockaddr_in remaddr;      /* server address */
-		socklen_t addrlen;
-		int recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
-		if (recvlen > 0) {
-			struct pbuf *rembuf = pbuf_alloc(PBUF_TRANSPORT, recvlen, PBUF_POOL);
-			struct pbuf *mybuf = rembuf;
-			if (mybuf == NULL)
-			{
-				printf("cannot alloc pbuf.\n");
-				return -1;
-			}
-
-			int copy_len = 0;
-			while (mybuf != NULL)
-			{
-				memcpy(mybuf->payload, buf+copy_len, mybuf->len);
-				copy_len += mybuf->len;
-				mybuf = mybuf->next;
-			}
-
-			tcp_input(rembuf);
-		}
-
-		tcp_tmr();
-	}
+	do {
+	    update();
+	} while (run_flag);
 
 	return 0;
 }
 
-
-void tcp_timer_needed(void)
-{
-}
-
-
-err_t ip_output_if(struct pbuf *p)
-{
-	return sendto(fd, p->payload, p->len, 0, (struct sockaddr *)&svraddr, sizeof(svraddr));
-}
-
+const static char* ECHO_STR = "hello world!";
 err_t connected(void *arg, struct tcp_pcb *tpcb, err_t err)
 {
 	printf("connected\n");
+
+	err_t wr_err = tcp_write(tpcb, ECHO_STR, strlen(ECHO_STR), 1);
+	if (wr_err != ERR_OK)
+	{
+	    printf("tcp_write failed, ret=%d", wr_err);
+	    tcp_close(tpcb);
+	    return wr_err;
+	}
+
 	return ERR_OK;
 }
 
+err_t
+echo_recv(void *arg, struct tcp_pcb *tpcb, struct pbuf *p, err_t err)
+{
+  assert (err == ERR_OK);
 
+  if (p != NULL)
+  {
+      const int BUFSIZE = 64*1024;
+      char buf[BUFSIZE];
+      int copy_len = 0;
+      while (p != NULL)
+      {
+          memcpy(buf+copy_len, p->payload, p->len);
+          copy_len += p->len;
+          p = p->next;
+      }
+      printf("recv: %s\n", buf);
+      tcp_recved(tpcb, copy_len);
+
+      tcp_close(tpcb);
+  }
+  else
+  {
+      printf("closed\n");
+      // closed
+      run_flag = false;
+  }
+
+  return ERR_OK;
+}
--- test/udp/server.cpp
+++ test/udp/server.cpp
@@ -9,6 +9,8 @@
 #include "lwip/memp.h"
 #include "lwip/pbuf.h"
 
+#include <assert.h>
+#include <errno.h>
 #include <sys/socket.h>
 //#include <netinet/in.h>
 #include <netinet/ip.h> /* superset of previous */
@@ -16,79 +18,49 @@
 #include <stdio.h>
 #include <string.h>
 
-int fd = -1;
-struct sockaddr_in remaddr;     /* remote address */
-socklen_t addrlen = 0;
+#include "echo.h"
+
+#include "common.h"
+
+int
+init_udp_svr()
+{
+    int fd = init_udp();
+    if (fd < 0)
+        return -1;
+
+    struct sockaddr_in myaddr;      /* our address */
+    /* bind the socket to any valid IP address and a specific port */
+    memset((char *)&myaddr, 0, sizeof(myaddr));
+    myaddr.sin_family = AF_INET;
+    myaddr.sin_addr.s_addr = inet_addr("10.12.21.11");
+    myaddr.sin_port = htons(448);
+
+    if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
+        perror("bind failed");
+        return -1;
+    }
+
+    return 0;
+}
 
 int main(int argc, const char* argv[])
 {
-	fd = socket(AF_INET, SOCK_DGRAM, 0);
-	if (fd < 0)
-	{
-		printf("cannot create socket\n");
-		return 0;
-	}
+    int ret = init_udp_svr();
+    if (ret != 0)
+        return 1;
 
-	struct sockaddr_in myaddr;      /* our address */
-	/* bind the socket to any valid IP address and a specific port */
-	memset((char *)&myaddr, 0, sizeof(myaddr));
-	myaddr.sin_family = AF_INET;
-	myaddr.sin_addr.s_addr = inet_addr("10.12.21.11");
-	myaddr.sin_port = htons(443);
+    //
+    init_tcp_stack();
 
-	if (bind(fd, (struct sockaddr *)&myaddr, sizeof(myaddr)) < 0) {
-			perror("bind failed");
-			return 0;
-	}
+    echo_init();
 
-	//初始化
-	tcp_init();
-	pbuf_init();
-	memp_init();
+    /* now loop, receiving data and printing what we received */
+    for (;;) {
+        update();
+    }
 
-	//
-	struct tcp_pcb *listen = tcp_new();
-    listen->local_port = 443;
-	tcp_listen(listen);
-
-	/* now loop, receiving data and printing what we received */
-	const int BUFSIZE = 64*1024;
-	char buf[BUFSIZE];
-	for (;;) {
-		int recvlen = recvfrom(fd, buf, BUFSIZE, 0, (struct sockaddr *)&remaddr, &addrlen);
-		if (recvlen > 0) {
-			struct pbuf *mybuf = pbuf_alloc(PBUF_TRANSPORT, recvlen, PBUF_POOL);
-			if (mybuf == NULL)
-			{
-				printf("cannot alloc pbuf.\n");
-				return -1;
-			}
-
-			int copy_len = 0;
-			struct pbuf *tmp_buf = mybuf;
-			while (tmp_buf != NULL)
-			{
-				memcpy(tmp_buf->payload, buf+copy_len, mybuf->len);
-				copy_len += tmp_buf->len;
-				tmp_buf = tmp_buf->next;
-			}
-
-			tcp_input(mybuf);
-		}
-
-		tcp_tmr();
-	}
-
-	return 0;
+    return 0;
 }
 
 
-void tcp_timer_needed(void)
-{
-}
-
-
-err_t ip_output_if(struct pbuf *p)
-{
-	return sendto(fd, p->payload, p->len, 0, (struct sockaddr *)&remaddr, addrlen);
-}
